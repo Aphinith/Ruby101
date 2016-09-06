dd375110f3dc7697ea25ccb3934145e6451b83d6
Aralya Phinith <aphinith84@gmail.com>
2016-09-05 18:43:40 -0700

    practice string interpolation

---
+++ string_interpolation.rb
@@ -0,0 +1,10 @@
+puts "Please enter your name"
+name = gets.chomp 
+
+puts "Please enter location"
+location = gets.chomp
+
+puts "Please enter your email"
+email = gets.chomp
+
+p "Your name is #{name} and your location is #{location} and you can be reached at #{email}."
