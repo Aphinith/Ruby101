1f62bea5b0b15a4d700f011e1577b95db5b1fc99
Aralya Phinith <aphinith84@gmail.com>
2016-09-05 22:14:18 -0700

    practice with class method instantiation vs instance method instantiation

---
+++ class.rb
@@ -0,0 +1,18 @@
+class Invoice
+  #Class method
+  def self.print_out
+    p "Print out invoice."
+  end
+
+  def convert_to_pdf
+    p "Converted to PDF."
+  end
+
+end
+
+Invoice.print_out
+
+i = Invoice.new
+i.convert_to_pdf
+
+# i.print_out
