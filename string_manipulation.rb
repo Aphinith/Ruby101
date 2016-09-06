6fc6b33c47141cf0db47b10773de04738523f9f1
Aralya Phinith <aphinith84@gmail.com>
2016-09-05 18:59:10 -0700

    practice string manipulation using methods

---
+++ string_manipulation.rb
@@ -0,0 +1,5 @@
+name = "Aralya Phinith"
+
+p "This is using .upcase: #{name.upcase} and this is using .lowcase: #{name.downcase}."
+p "This is using .swapcase: #{name.swapcase}."
+p "this is using .reverse and chaining it with .swapcase: #{name.reverse.swapcase}."
