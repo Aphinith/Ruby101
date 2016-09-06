4c77a916d748fdbb99fceaf8534e00e064d4b011
Aralya Phinith <aphinith84@gmail.com>
2016-09-05 23:12:42 -0700

    more practice with while loops

--- loops.rb
+++ loops.rb
@@ -1,6 +1,11 @@
 i = 0
 
-while i < 10
-  puts "Printing out because i is #{i}."
+while i <= 10
+  if i != 10
+    puts "Printing out because i is #{i}."
+  end
+  if i == 10
+    puts "This is the last output because i is finally #{i}."
+  end
   i += 1
 end
