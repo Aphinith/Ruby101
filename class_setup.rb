381706b3b301fd867f41f6b2369fd405f4d0280c
Aralya Phinith <aphinith84@gmail.com>
2016-09-09 14:29:10 -0700

    practice with initialize using class

--- class_setup.rb
+++ class_setup.rb
@@ -4,10 +4,22 @@ class ApiConnector
   def test_method
     puts "Calling test_method"
   end
+
+  def initialize(title, description, url)
+    @title = title
+    @description = description
+    @url = url
+    puts "Initialize method called cuz this was instantiated"
+  end
+
+  def test_intialize
+    puts @title
+    puts @description
+    puts @url
+  end
+
 end
 
-api = ApiConnector.new
+api = ApiConnector.new("League of Legends", "Most epic fighting game ever", "https://LeagueOfLegends.com")
 
-api.url = "http://google.com/"
-
-api.test_method
+api.test_intialize
