821d09394413a8974d571620d0e3378ba1de64ef
Aralya Phinith <aphinith84@gmail.com>
2016-09-09 13:35:13 -0700

    creating hashes using three different syntax methods

---
+++ hashes.rb
@@ -0,0 +1,21 @@
+hash1 = {
+  first_value: "The first one entered",
+  second_value: "The second value entered",
+  third_value: "The third value entered"
+}
+
+print "This is hash1: \n#{hash1} \n"
+
+hash_rockets = {
+  "Rockets1" => "BOOM IT GOES!",
+  "Rockets2" => "POW IT FLIES!"
+}
+
+print "This is using hash_rockets: \n#{hash_rockets} \n"
+
+hash_using_symbols = {
+  :symbol1 => "HOO BOY!",
+  :symbol2 => "YOWZERS"
+}
+
+print "This is using symbols: \n#{hash_using_symbols} \n"
